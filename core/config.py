c1addd7f693655f335352df41c5d4dfe7f5834c6
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-04-25 18:56:16 +0300

    add default username, email, password for createsuperuser

--- core/config.py
+++ core/config.py
@@ -2,22 +2,28 @@ from core import py_env
 
 SECRET_KEY = py_env.SECRET_KEY
 
-# Token 60 minutes * 24 hours * 8 days = 8 days
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
-
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql://{py_env.POSTGRES_USER}:{py_env.POSTGRES_PASSWORD}@{py_env.POSTGRES_SERVER}/{py_env.POSTGRES_DB}"
 )
 
+# settings for jwt authentication
 TOKEN_TYPE = py_env.TOKEN_TYPE
 ALGORITHM = py_env.ALGORITHM
 ACCESS_TOKEN_JWT_SUBJECT = py_env.ACCESS_TOKEN_JWT_SUBJECT
+# Token 60 minutes * 24 hours * 8 days = 8 days
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
 
+# settings for smtp
 EMAIL_HOST = py_env.EMAIL_HOST
 EMAIL_HOST_PASSWORD = py_env.EMAIL_HOST_PASSWORD
 EMAIL_HOST_USER = py_env.EMAIL_HOST_USER
 EMAIL_PORT = py_env.EMAIL_PORT
 EMAIL_USE_TLS = py_env.EMAIL_USE_TLS
 EMAIL_USERNAME = py_env.EMAIL_USERNAME
+
+USERNAME_SUPERUSER = py_env.USERNAME_SUPERUSER
+EMAIL_SUPERUSER = py_env.EMAIL_SUPERUSER
+PASSWORD_SUPERUSER = py_env.PASSWORD_SUPERUSER
+REPEAT_PASSWORD_SUPERUSER = py_env.REPEAT_PASSWORD_SUPERUSER
 
 # ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50))
