cc5a35c939604e8abfc6b73b69d1e39d26d3a624
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-04-28 00:40:26 +0300

    rename router

--- routers.py
+++ routers.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
 from apps.user import views as user_view
-from apps.token import views as token_views
+from apps.token.views import auth_router
 
 routers = APIRouter()
 
 routers.include_router(user_view.router, prefix='')
-routers.include_router(token_views.router, prefix='')
+routers.include_router(auth_router, prefix='')
